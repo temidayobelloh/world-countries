10c2d0d4971abdad45f08c3db491e325bf8c8b20
temidayobelloh <temidayobelloh@gmail.com>
2024-11-22 03:22:52 -0800

    implemented the navigation menu

---
+++ src/Components/Home/home.jsx
@@ -0,0 +1,13 @@
+import './home.css';
+import NavBar from '../NavBar/navBar';
+
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/Components/NavBar/navBar.jsx
@@ -0,0 +1,15 @@
+import './navBar.css';
+import darkMode from '../Assets/dark_mode_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg';
+
+const NavBar = () => {
+  return (
+    <div>
+    <nav className='navBar'>
+    <h2>Where in the world?</h2>
+    <p><button><img src={darkMode} alt={darkMode}/></button>Dark Mode</p>
+    </nav>
+    </div>
+  )
+}
+
+export default NavBar;
